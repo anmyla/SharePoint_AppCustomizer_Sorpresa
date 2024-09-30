830a8385a43b820b0ba2662d63cc3b24600b7949
myla <myla.ariaga@tecconsult.at>
2024-09-30 21:12:07 +0200

    if event is ongoing, render elemets

--- src/extensions/sorpresa/SorpresaApplicationCustomizer.tsx
+++ src/extensions/sorpresa/SorpresaApplicationCustomizer.tsx
@@ -15,6 +15,140 @@ import "@pnp/sp/items";
 import "@pnp/sp/site-users/web";
 
 import 'animate.css';
+import CustomAlert from './components/Gifts/CustomAlert';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const LOG_SOURCE: string = 'SorpresaApplicationCustomizer';
+
+export interface ISorpresaApplicationCustomizerProperties {
+  Bottom: string;
+}
+
+export default class SorpresaApplicationCustomizer
+  extends BaseApplicationCustomizer<ISorpresaApplicationCustomizerProperties> {
+
+  private sp: ReturnType<typeof spfi>;
+  private _bottomPlaceholder: PlaceholderContent | undefined;
+
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+    this.sp = spfi().using(SPFx(this.context));
+    this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
+    return Promise.resolve();
+  }
+
+  private _renderPlaceHolders(): void {
+    console.log("SorpresaApplicationCustomizer._renderPlaceHolders()");
+
+    if (!this._bottomPlaceholder) {
+      this._bottomPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Bottom,
+        { onDispose: this._onDispose }
+      );
+
+      if (!this._bottomPlaceholder) {
+        console.error("The expected placeholder (Bottom) was not found.");
+        return;
+      }
+
+      if (this.properties && this._bottomPlaceholder.domElement) {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const imgSrc = require('./assets/img/egg1.png');
+
+        this._bottomPlaceholder.domElement.innerHTML = `
+          <div class="${styles.app}">
+            <div class="${styles.bottom}">
+              <div class="${styles.giftBox}" id="giftBox">
+                <img src="${imgSrc}" id="${styles.giftImage}" class="animate__animated animate__bounce animate__repeat-3" alt="Surprise!" />
+              </div>
+            </div>
+            <div id="customAlertContainer"></div>
+          </div>
+        `;
+
+        const giftBox = this._bottomPlaceholder.domElement.querySelector("#giftBox");
+        if (giftBox) {
+          giftBox.addEventListener("click", () => this._showAlertCustomBox());
+        }
+      }
+    }
+  }
+
+  private _onDispose(): void {
+    console.log('[SorpresaApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
+    if (!this._bottomPlaceholder) {
+      console.error('The expected placeholder (Bottom) was not found.');
+      return;
+    }
+    ReactDOM.unmountComponentAtNode(this._bottomPlaceholder.domElement);
+  }
+
+  private async _showAlertCustomBox(): Promise<void> {
+    try {
+      const winner = await this.sp.web.currentUser();
+
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      const handleSave = async () => {
+        try {
+          const currentURL = "https://tecconsultat.sharepoint.com/sites/Myla/SiteCustomiser/SitePages/Home.aspx";
+
+          const item = await this.sp.web.lists.getByTitle("SorpresaWinners").items.add({
+            Title: winner.Title,
+            Name: winner.Title,
+            Email: winner.Email,
+            Website: currentURL.toString(),
+          });
+
+          console.log("WINNER DATA Listed: " + JSON.stringify(item, null, 2));
+        } catch (error) {
+          console.error("Error adding the item to the list: ", error);
+        }
+      };
+
+      const customAlertElement: React.ReactElement = React.createElement(CustomAlert, {
+        userName: winner.Title,
+        onSave: handleSave
+      });
+
+      const alertContainer = this._bottomPlaceholder?.domElement?.querySelector("#customAlertContainer");
+
+      if (alertContainer) {
+        ReactDOM.render(customAlertElement, alertContainer);
+      } else {
+        console.error("Alert container not found. Unable to render the alert.");
+      }
+
+    } catch (error) {
+      console.error("Error fetching the current user: ", error);
+    }
+  }
+}
+
+
+
+
+/*
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseApplicationCustomizer,
+  PlaceholderContent,
+  PlaceholderName
+} from '@microsoft/sp-application-base';
+
+import * as strings from 'SorpresaApplicationCustomizerStrings';
+import styles from './AppCustomizer.module.scss';
+
+import { spfi, SPFx } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import "@pnp/sp/site-users/web";
+
+import 'animate.css';
+import CustomAlert from './components/Gifts/CustomAlert';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 
 const LOG_SOURCE: string = 'SorpresaApplicationCustomizer';
@@ -72,16 +206,61 @@ export default class SorpresaApplicationCustomizer
 
           const giftImage = this._bottomPlaceholder.domElement.querySelector("#giftBox");
           if (giftImage) {
-            giftImage.addEventListener("click", () => this._handleGiftClick());
+            giftImage.addEventListener("click", () => this._showAlertCustomBox());
           }
         }
       }
     }
   }
 
+
   private _onDispose(): void {
     console.log('[SorpresaApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
+    if (!this._bottomPlaceholder) {
+      console.error('The expected placeholder (Bottom) was not found.');
+      return;
+    }
+    ReactDOM.unmountComponentAtNode(this._bottomPlaceholder.domElement);
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+private async _showAlertCustomBox(): Promise<void> {
+    try {
+      const winner = await this.sp.web.currentUser();
+  
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      const handleSave = async () => {
+        try {
+          const currentURL = "https://tecconsultat.sharepoint.com/sites/Myla/SiteCustomiser/SitePages/Home.aspx";
+          
+          const item = await this.sp.web.lists.getByTitle("SorpresaWinners").items.add({
+            Title: winner.Title,
+            Name: winner.Title,
+            Email: winner.Email,
+            Website: currentURL.toString(),
+          });
+  
+          console.log("WINNER DATA Listed: " + JSON.stringify(item, null, 2));
+        } catch (error) {
+          console.error("Error adding the item to the list: ", error);
+        }
+      };
+  
+      const customAlertElement: React.ReactElement = React.createElement(CustomAlert, {
+        userName: winner.Title,
+        onSave: handleSave
+      });
+  
+      ReactDOM.render(customAlertElement, this._bottomPlaceholder.domElement);
+  
+    } catch (error) {
+      console.error("Error fetching the current user: ", error);
+      // Handle the error or show an error message in the UI
+    }
+  }
+  
+
+
 
   private async _handleGiftClick(): Promise<void> {
     const user = await this.sp.web.currentUser();
@@ -113,6 +292,6 @@ export default class SorpresaApplicationCustomizer
       };
     }
   }
-}
+    */
 
 
--- src/extensions/sorpresa/components/Gifts/CustomAlert.tsx
+++ src/extensions/sorpresa/components/Gifts/CustomAlert.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import AlertButton from './Button';
 
-
 interface CustomAlertProps {
   userName: string;
   onSave: () => void;
@@ -16,31 +15,30 @@ export default class CustomAlert extends React.Component<CustomAlertProps, Custo
   constructor(props: CustomAlertProps) {
     super(props);
 
-    // Initialize the state
+
     this.state = {
       message: `Congratulations ${this.props.userName}, you just found one of those hidden gifts!`,
       buttonText: 'OK',
     };
 
-    // Bind the handleClick method
+
     this.handleClick = this.handleClick.bind(this);
   }
 
-  // Handle the button click event
+
   handleClick(): void {
     if (this.state.buttonText === 'OK') {
-      // Update state when the button text is 'OK'
       this.setState({
         message: "We've recorded your details to the winners list!",
         buttonText: 'Close',
       });
     } else {
-      // Call the onSave callback prop when button text is 'Close'
+
       this.props.onSave();
     }
   }
 
-  // Render the JSX
+
   public render(): JSX.Element {
     return (
       <div
@@ -53,6 +51,7 @@ export default class CustomAlert extends React.Component<CustomAlertProps, Custo
         }}
       >
         <p id="customAlertMessage">{this.state.message}</p>
+        {/* Render AlertButton with dynamic button text */}
         <AlertButton title={this.state.buttonText} onSave={this.handleClick} />
       </div>
     );
